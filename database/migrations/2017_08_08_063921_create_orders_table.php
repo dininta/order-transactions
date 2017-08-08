1ff058350830bf6154775d0e58bb5e594ce1a8b9
dininta <dinintaannisa@yahoo.com>
2017-08-08 13:59:51 +0700

    Create database migration

---
+++ database/migrations/2017_08_08_063921_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->tinyInteger('status');
+            $table->string('reason')->nullable();
+            $table->string('payment_proof', 500);
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->string('phone_number', 50);
+            $table->string('address');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
