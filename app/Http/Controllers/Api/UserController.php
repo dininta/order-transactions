ceda515ee2905aebc00975006557623db721e4fd
dininta <dinintaannisa@yahoo.com>
2017-08-09 19:33:37 +0700

    Add token authentication

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Model\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use JWTAuth;
+use Response;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'email' => 'required|exists:users,email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 401,
+                'message' => $validator->errors(),
+                'result' => null
+            ]);
+        }
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if ($token = JWTAuth::attempt($credentials)) {
+            return response()->json(compact('token'));
+        }
+
+        return response()->json([
+            'status' => 404,
+            'message' => 'Failed to authenticate user',
+            'result' => null
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function () {
+    Route::post('login', [ 'uses' => 'UserController@login']);
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        
+    });
 });
